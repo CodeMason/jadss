cd591701e33200de6cd4df388047c646a79342f6
norhild85 <norhild85@5858ee4c-f922-11dd-a295-0b7b0935bf94>
2009-03-16 13:42:29 +0000

    Fixed some issues with measuring the amount of bytes (and consequently of µs)
    Also, the main play bucle has become a for (seems to work better due to optimisation, and it's easier to manage)
    
    Mostly commented (at least most important methods of Sound
    
    It's still a developement version, so there's no GUI and there are some printout left for debugging.

--- trunk/jadss/src/synth/Main.java
+++ trunk/jadss/src/synth/Main.java
@@ -16,14 +16,15 @@ public class Main {
 	 * @throws InterruptedException 
 	 */
 	public static void main(String[] args) throws InterruptedException {
-		Sound s = new Sound("sounds/alert.wav");
+		Sound s = new Sound("sounds/stillalive.wav");
 		s.start();
-		s.setChunkSize(16);
-		s.setDistance(12f);
+		s.setChunkSize(512);
+		s.setDistance(10f);
 		System.out.println("Offset: "+s.getOffsetMicros()+" us ("+s.getOffset()+" B), MAX: "+s.getMaximumDistance()+" m");		
 		System.out.println("Play at: "+s.getDistance()+ " m (" + s.getGain()+ " dB)");
 		System.out.println("Bytes Per second: "+s.getBytesSec()+" B/s ,  bits per sample: "+s.getBitsPerSample()+" bps , chunksize: "+s.getChunkSize()+" B");
-		s.playFromTo(0, 1000000);
+		//s.playFromTo(0, 2000000);
+		s.play();
 		System.out.println("Offset: "+s.getOffsetMicros()+" us ("+s.getOffset()+" B)");		
 
 		s.close();
