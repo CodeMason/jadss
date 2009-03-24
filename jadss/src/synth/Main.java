16e3ccaac12adf3ff4d18166b8c31bdc3b1cac41
norhild85 <norhild85@5858ee4c-f922-11dd-a295-0b7b0935bf94>
2009-03-24 14:25:04 +0000

    Little folders modifications, and added a pair of pics for documentation

--- jadss/src/synth/Main.java
+++ jadss/src/synth/Main.java
@@ -16,21 +16,22 @@ public class Main {
 	public static void main(String[] args) throws InterruptedException {
 		Sound s = new Sound("sounds/stillalive.wav");
 		s.start();
-		s.setChunkSize(8192);
-		s.setDistance(5f);
+		s.setDistance(12);
 		System.out.println("Offset: "+s.getOffsetMicros()+" us ("+s.getOffset()+" B), MAX: "+s.getMaximumDistance()+" m");		
 		System.out.println("Play at: "+s.getDistance()+ " m (" + s.getGain()+ " dB)");
 		System.out.println("Bytes Per second: "+s.getBytesSec()+" B/s ,  bits per sample: "+s.getBitsPerSample()+" bps , chunksize: "+s.getChunkSize()+" B");
 		long i,e;
-		i = System.nanoTime();
-		long dur= 5000000;
-		s.playFromTo(0,dur);
-		e = System.nanoTime();
-		System.out.println("Real duration: "+((e-i)/1000)+" us\t Percentage: "+ (((e-i)/1000.f)/dur*100)+" %");
+		i = System.currentTimeMillis();
+		long dur   = 10000000;
+		long start = s.getOffsetMicros();
+		s.playFromTo(start,dur);
+		e = System.currentTimeMillis();
+		System.out.println("Duration: "+(dur-start)/1000.f+" Real duration: "+((e-i))+" us\t Percentage: "+ (((float)(e-i))/(dur-start)*100000)+" %");
 		System.out.println("Offset: "+s.getOffsetMicros()+" us ("+s.getOffset()+" B)");		
-
+		Thread.sleep(1000);
+		
+		System.out.println("Bye!");
 		s.close();
-
 	}
 	
 }
