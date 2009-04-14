ff8ec7255156db49f804c0a1ad33a119197dc08a
norhild85 <norhild85@5858ee4c-f922-11dd-a295-0b7b0935bf94>
2009-04-14 09:32:58 +0000

    Modified buffers.png Added comments and modified some methods in order to increase integrity and stability.

--- trunk/jadss/src/synth/Main.java
+++ trunk/jadss/src/synth/Main.java
@@ -14,7 +14,7 @@ public class Main {
 	 * @throws InterruptedException 
 	 */
 	public static void main(String[] args) throws InterruptedException {
-		test1();
+		test2();
 	}
 	
 	public static void test1(){
@@ -29,9 +29,9 @@ public class Main {
 		System.out.println("AudioSize: "+(s.getAudioLength()/1000)+" ms ("+ s.getAudioSize()+" B)  Prec: "+ s.getPrecision()+" s");
 		System.out.println("Offset: "+s.getOffsetMicros()+" us ("+s.getOffset()+" B), MAX: "+s.getMaximumDistance()+" m");		
 		System.out.println("Play at: "+s.getDistance()+ " m (" + s.getGain()+ " dB)");
-		System.out.println("Bytes Per second: "+s.getBytesSec()+" B/s ,  bits per sample: "+s.getBitsPerSample()+" bps , chunksize: "+s.getChunkSize()+" B");
+		System.out.println("Bytes Per second: "+s.getBytesPerSec()+" B/s ,  bits per sample: "+s.getBitsPerSample()+" bps , chunksize: "+s.getChunkSize()+" B");
 		
-		for(int j=32;j<=512;j*=2){
+		for(int j=1;j<=512;j*=2){
 			s.setChunkSize(16*j);
 			int dur   = 10000;//s.getChunkSize();
 			ini = System.currentTimeMillis();
@@ -48,6 +48,44 @@ public class Main {
 			System.out.println(s.getChunkSize()+"\t"+(((float)(end-ini))/(dur*times)*100000));
 		}
 		
+
+		System.out.println("Bye!");
+		s.close();
+	}
+
+
+	public static void test2(){
+
+
+		long i,ini,e,end;
+		int times;
+
+		Sound s = new Sound("sounds/stillalive.wav");
+		s.start();
+		s.setDistance(6.032f);
+
+
+		System.out.println("AudioSize: "+(s.getAudioLength()/1000)+" ms ("+ s.getAudioSize()+" B)  Prec: "+ s.getPrecision()+" s");
+		System.out.println("Offset: "+s.getOffsetMicros()+" us ("+s.getOffset()+" B), MAX: "+s.getMaximumDistance()+" m");		
+		System.out.println("Play at: "+s.getDistance()+ " m (" + s.getGain()+ " dB)");
+		System.out.println("Bytes Per second: "+s.getBytesPerSec()+" B/s ,  bits per sample: "+s.getBitsPerSample()+" bps , chunksize: "+s.getChunkSize()+" B");
+
+		int dur   = 6825;//s.getChunkSize();
+		ini = System.currentTimeMillis();
+		for(times=0;times<800;times++){
+			s.setDistance(times/800.f*12+0.1f);
+			i = System.currentTimeMillis();
+
+			s.playDuringMicros(dur*times, dur);
+			//s.playDuring(dur*times, dur);
+			e = System.currentTimeMillis();
+			//			System.out.println("Duration: "+(dur)/1000.f+" ms Real duration: "+((e-i))+" ms\t Percentage: "+ (((float)(e-i))/(dur)*100000)+" %");
+			//			System.out.println("Offset: "+s.getOffsetMicros()+" us ("+s.getOffset()+" B)");
+		}
+		end = System.currentTimeMillis();/*+*/
+		System.out.println(times+"\t"+dur+"\t"+(dur*times)/1000.f+"\t"+(end-ini)+"\t"+(((float)(end-ini))/(dur*times)*100000));
+
+
 
 		System.out.println("Bye!");
 		s.close();
--- trunk/jadss/src/synth/Sound.java
+++ trunk/jadss/src/synth/Sound.java
@@ -18,17 +18,21 @@ import javax.sound.sampled.UnsupportedAudioFileException;
  * Sound class. It can be controlled in data bytes or time and offers a simple compensation of distance (in gain and delay).
  */
 public class Sound{
+	/** Default value for ChunkSize	 */
+	public static int DEFAULT_CHUNKSIZE = 4096;
+	/** Default value for lineSize	 */
+	public static int DEFAULT_LINESIZE = 4096;
 
 	/** Sample data array */
 	private byte data[];
-	
+
 	/**Size of each chunk that will be written in line
 	 *    If it's too small, sound will play with artifacts due to the pauses between writings (buffer underrun).
 	 *    If it's too big, the precision will decrease*/
-	private int chunkSize = 1024;
+	private int chunkSize = DEFAULT_CHUNKSIZE;
 	
 	/**Size of array bytes for sampling*/
-	private int playSampleCount = chunkSize;
+	private int lineSize = chunkSize;
 	
 	/**Sound offset in bytes used for both sounds, files and 'sampled'*/
 	private int offset = 0;
@@ -78,7 +82,7 @@ public class Sound{
 
 
 	/**
-	 * Default constructor, generates a Sin wave sound sampled directly
+	 * Default constructor, generates a Sin wave sound created mathematically.
 	 */
 	public Sound(){
 		data = new byte[(int)(defaultRate)];
@@ -99,7 +103,7 @@ public class Sound{
 	    
 	    try {
 			line = (SourceDataLine) AudioSystem.getLine(info);
-			line.open(format, playSampleCount);
+			line.open(format, lineSize);
 			gainControl = (FloatControl) line.getControl( FloatControl.Type.MASTER_GAIN);
 			gainControl.setValue(refGain);
 
@@ -110,13 +114,23 @@ public class Sound{
 
 	}
 	
+	/**
+	 * Constructor with parameters we will use in case we need to play a sound file.
+	 * @param filename String that contains the path to the file we will use.
+	 */	
+	public Sound(String filename){
+		this(filename,DEFAULT_CHUNKSIZE,DEFAULT_LINESIZE);
+	}
 	
 	/**
-	 * Constructor with parameters we will use in case we need to play a sound file
-	 * @param filename String that contains the path to the file we will use
+	 * Constructor with parameters we will use in case we need to play a sound file.
+	 * @param filename String that contains the path to the file we will use.
+	 * @param cSize int containing the chunk buffer size desired.
+	 * @param lSize int containing the line size desired.
 	 */
-	public Sound(String filename){
-
+	public Sound(String filename, int cSize, int lSize){
+		this.chunkSize = cSize;
+		this.lineSize = lSize;
 		File f = new File(filename);
 		try {
 			audioInputStream = AudioSystem.getAudioInputStream(f);
@@ -157,7 +171,7 @@ public class Sound{
 		info = new DataLine.Info(SourceDataLine.class, format);
 		try {
 			line = (SourceDataLine) AudioSystem.getLine(info);
-			line.open(format, playSampleCount);
+			line.open(format, lineSize);
 			gainControl = (FloatControl) line.getControl( FloatControl.Type.MASTER_GAIN);
 			gainControl.setValue(refGain);
 		} catch (LineUnavailableException e) {
@@ -175,125 +189,187 @@ public class Sound{
 
 
 	//GETTERS AND SETTERS	
+	
+	/**
+	 * @return Size of the audio sample in bytes.
+	 */
 	public int getAudioSize() {
 		return audioSize;
 	}
 	
+	/**
+	 * @return Length of audio expressed in microseconds.
+	 */
 	public long getAudioLength(){
 		return (long) ((1000000.f*audioSize)/bytesSec);
 	}
 
-	public int getPlaySampleCount() {
-		return playSampleCount;
+	/**
+	 * @return Size of line buffer.
+	 */
+	public int getLineSize() {
+		return lineSize;
 	}
 
-	public void setPlaySampleCount(int playSampleCount) {
-		this.playSampleCount = playSampleCount;
-	}
 
+	/**
+	 * @return Default audio rate (size in bytes of sampled file).
+	 */
 	public float getDefaultRate() {
 		return defaultRate;
 	}
 	
+	/**
+	 * @return Number of bits in a sample.
+	 */
 	public int getBitsPerSample(){
 		return bitsPerSample;
 	}
 
-	public void setDefaultRate(float defaultRate) {
-		this.defaultRate = defaultRate;
-	}
 
+	/**
+	 * @return Chunk buffer size. 
+	 */
 	public int getChunkSize() {
 		return chunkSize;
 	}
 
+	/**
+	 * @param chunkSize new size of chunk buffer. 
+	 */
 	public void setChunkSize(int chunkSize) {
 		this.chunkSize = chunkSize;
 	}
 
-	public long getBytesSec() {
+	/**
+	 * @return Bytes Per Second.
+	 */
+	public long getBytesPerSec() {
 		return bytesSec;
 	}
 
+	/**
+	 * @return Reference Audio Gain.
+	 */
 	public float getRefGain() {
 		return refGain;
 	}
 
+	/**
+	 * @return Reference Audio Distance.
+	 */
 	public float getRefDistance() {
 		return refDistance;
 	}
 
+	/**
+	 * @param refDistance Float containing the new reference distance. Calls setDistance with actual distance.
+	 */
 	public void setRefDistance(float refDistance){
 		this.refDistance = refDistance;
 		this.setDistance(distance);
 	}
 	
+	/**
+	 * @param refGain Float containing the new reference gain. Calls setDistance with actual distance.
+	 */
 	public void setRefGain(float refGain) {
 		this.refGain = refGain;
 		gainControl.setValue(this.refGain);
 		//Update the gain and delay
 		this.setDistance(distance);
-	}
-
+	}	
+	
+	/**
+	 * @return Calculated gain in the actual distance.
+	 */
 	public float getGain() {
 		return gain;
 	}
 
+	/**
+	 * @param gain Float containing the new value for the actual gain.
+	 */
 	public void setGain(float gain) {
 		this.gain = gain;
+		gainControl.setValue(gain);
 	}
 
+	
+	/**
+	 * @return Actual distance from sound source.
+	 */
 	public float getDistance() {
 		return distance;
 	}
 	
 	
 
+	/**
+	 * Set the distance (from the sound source) to be simulated.
+	 * @param meters New distance expressed in meters.
+	 */
 	public void setDistance(float meters){
 		//VOLi = VOLRef - 19.93 log10(di / max(di = 1,..., 4))
 		//TDi = [max(di=1,..,4) - di] / Vs
 		
 		//If distance is bigger than we can allow, we need to adjust it 
 		// (maybe in future we should make refGain change dynamically along the distance) 
-		if(meters<getMaximumDistance()){
-			distance = meters;
-			gain = (float)(19.93f*Math.log10(distance/refDistance));
-			gain+=refGain;
-			gainControl.setValue(gain);
+		if(Math.abs(meters-distance)>0.001){
+			if(meters<getMaximumDistance()){
+				distance = meters;
+				gain = (float)(19.93f*Math.log10(distance/refDistance));
+				gain+=refGain;
+				gainControl.setValue(gain);
+			}
+			else {
+				System.err.println("Distance input is higher than maximum distance: Adjusting to maximum");
+				distance = getMaximumDistance();
+				gain = gainControl.getMaximum();
+				gainControl.setValue(gain);
+			}
+			float f =(getOffsetMicros()+(distance-refDistance)/343*1000000);
+			setOffsetMicros((long)f);
 		}
-		else {
-			System.err.println("Distance input is higher than maximum distance: Adjusting to maximum");
-			distance = getMaximumDistance();
-			gain = gainControl.getMaximum();
-			gainControl.setValue(gain);
-		}
-		this.offset=0;
-		float f =(getOffsetMicros()+(distance-refDistance)/343*1000000);
-		setOffsetMicros((long)f);
 	}
 
+	/**
+	 * @return Position within the audio, expressed in bytes.
+	 */
 	public int getOffset() {
 		return offset;
 	}
 
+	/**
+	 * Establish the new initial playing position offset.
+	 * @param off new offset.
+	 */
 	public void setOffset(int off) {
-		this.offset = off;
-		if(off>=audioSize){
-			this.offset=off%audioSize;
-			System.err.println("Warning: Offset Bigger than audio size:( "+ off+" % "+audioSize+" = "+this.offset+" )");
-		}
-		if(off%frameSize!=0){
+		if(this.offset!=off){
+			this.offset = off;
+			if(off>=audioSize){
+				this.offset=off%audioSize;
+				System.err.println("Warning: Offset Bigger than audio size:( "+ off+" % "+audioSize+" = "+this.offset+" )");
+			}
+			if(off%frameSize!=0){
 				this.offset = this.offset +(this.offset%frameSize);
 				System.err.println("Warning, offset needs to match an integral frame size. Old value: "+off+" New value: "+ this.offset);
+			}
 		}
-		
 	}
 	
+	/**
+	 * @return offset from starting position in microseconds.
+	 */
 	public long getOffsetMicros(){
 		float f= ((1000000.f*offset)/bytesSec);
 		return (long) f;
 	}
 
+	/**
+	 * Sets the new audio offset position. 
+	 * @param off New offset expressed in microseconds.
+	 */
 	public void setOffsetMicros(long off){
 		float offsetBytes = (off*bytesSec)/1000000.f;
 		setOffset((int) offsetBytes);
@@ -301,12 +377,20 @@ public class Sound{
 	
 	
 	
+	/**
+	 * Gets the precision the system offers for changing distance
+	 * @return Precision expressed in microseconds.
+	 */
 	public float getPrecision(){
-		float result = ((float)chunkSize)/bytesSec;
+		float result = (1000000f*chunkSize)/bytesSec;
 		
 		return result;
 	}
 	
+	/**
+	 * Gets the maximum allowed distance for the current reference gain.
+	 * @return distance expressed in meters.
+	 */
 	public float getMaximumDistance() {
 		float max = gainControl.getMaximum();
 		
@@ -314,13 +398,16 @@ public class Sound{
 	}
 	
 	
+	/**
+	 * Start and open the soundcard line.
+	 */
 	public void start(){
 		line.start();
 	}
 	
 
 	/**
-	 * Play indefinitely a sound 
+	 * Play indefinitely a sound. This method never ends, since it enters in a infinite loop.
 	 */
 	public void loop(){
 		while(true){
@@ -333,9 +420,9 @@ public class Sound{
 
 	
 	/**
-	 * Play a sound during a certain amount of time. Uses play(start,end)
-	 * @param uStart starting time expressed in microseconds
-	 * @param uEnd end time expressed in microseconds
+	 * Play a sound between two time positions. Calls play(start,end).
+	 * @param uStart starting time expressed in microseconds.
+	 * @param uEnd end time expressed in microseconds.
 	 */
 	public void playFromTo(long uStart, long uEnd){
 		float f = (uStart*bytesSec)/1000000.f;
@@ -353,8 +440,8 @@ public class Sound{
 	
 	
 	/**
-	 * Play a sound during a certain amount of time. Uses playDuring(start,length)
-	 * @param uStart starting position time expressed in microseconds
+	 * Play a sound during a certain amount of time. Calls playDuring(start,length).
+	 * @param uStart starting position time expressed in microseconds.
 	 * @param uLength duration expressed in microseconds
 	 */
 	public void playDuringMicros(long uStart, long uLength){
@@ -368,9 +455,9 @@ public class Sound{
 	
 	
 	/**
-	 * Play a sound for duration.
-	 * @param start Starting byte
-	 * @param length Length of the play (expressed in bytes)
+	 * Play a sound during a determined duration.
+	 * @param start Starting byte.
+	 * @param length Length of the play (expressed in bytes).
 	 */
 	public void playDuring(int start, int length){				
 		int played=0;
@@ -426,8 +513,8 @@ public class Sound{
 	
 	/**
 	 * Play a sound between the bytes indicated.
-	 * @param start Starting byte position
-	 * @param end Ending byte position
+	 * @param start Starting byte position.
+	 * @param end Ending byte position.
 	 */
 	public void play(int start, int end){
 		setOffset(start%audioSize);
@@ -475,7 +562,7 @@ public class Sound{
 	
 	
 	/**
-	 * Play a complete sound, from the beginning to the end. 
+	 * Play a complete sound, from its beginning to its end (simulating the distance).
 	 */
 	public void play(){
 		while(offset<(audioSize-chunkSize)){
@@ -489,7 +576,7 @@ public class Sound{
 
 	
 	/**
-	 * Close the sound system
+	 * Close the sound system.
 	 */
 	public void close() {		
 		line.close();
